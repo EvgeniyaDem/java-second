57ec11ac044b3dbeb93569fbcfbe7042bdb7c19e
Evgeniya D. <Evgeniya7000@rambler.ru>
2024-02-01 15:05:20 +1200

    added code

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int price_ticket = 15000; /*цена билета*/
+        int price_mile = 20; /*цена 1 мили*/
+        int amount_mile;  /*количество начисленных миль*/
+        amount_mile = price_ticket / price_mile;
+        System.out.println("Количество начисленных бонусных миль:" + amount_mile);
     }
 }
